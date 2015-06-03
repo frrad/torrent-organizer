552ad323bc8c59fa6bf90531d9ec33b015e4d874
Frederick Robinson <frederick.robinson@gmail.com>
2015-06-02 21:05:37 -0700

    support multiple files

--- organize.py
+++ organize.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
-import shutil
 import bencode
 import glob
 import hashlib
 import os
+import shutil
 import sys
+import types
 
 
 def decode(path):
@@ -14,15 +15,29 @@ def decode(path):
     return answer
 
 
+def chunk_files(paths, size):
+    if isinstance(paths, types.StringTypes):
+        paths = [paths]
+
+    chunk = ''
+
+    for path in paths:
+        with open(path, 'r') as f:
+            while True:
+                chunk += f.read(size - len(chunk))
+                if len(chunk) == size:
+                    yield chunk
+                    chunk = ''
+                else:
+                    break
+
+    if chunk != '':
+        yield chunk
+
+
 def file_hash_list(path, piece_length):
-    hash_list = []
-
-    with open(path, 'r') as f:
-        chunk = f.read(piece_length)
-        while chunk != '':
-            hash_list.append(hashlib.sha1(chunk).digest())
-            chunk = f.read(piece_length)
-
+    hash_list = [hashlib.sha1(chunk).digest()
+                 for chunk in chunk_files(path, piece_length)]
     return ''.join(hash_list)
 
 
@@ -30,8 +45,61 @@ def extract_descriptor(path):
     return path[path.rfind('/') + 1:path.rfind('.')]
 
 
+def handle_multi_torrent(torrent_info, search_glob, destination_folder):
+
+    piece_length = torrent_info['info']['piece length']
+    series = []
+
+    for file_info in torrent_info['info']['files']:
+        possibles = find_potential_files(search_glob, file_info['length'])
+        if len(possibles) == 0:
+            print "Can't find any files matching", file_info['path'][0]
+            return
+
+        series.append(possibles)
+
+    sequences = [[]]
+    for spread in series:
+        trashcan = []
+        for part in spread:
+            for lid in sequences:
+                trashcan.append(lid + [part])
+        sequences = trashcan
+
+    for possibility in sequences:
+        matches = file_hash_list(
+            possibility, piece_length) == torrent_info['info']['pieces']
+        print "Found files", possibility
+        print 'Matches.' if matches else 'Doesn\'t match'
+        if matches:
+            print 'Creating directory', destination_folder
+            os.makedirs(destination_folder)
+
+            de, a = torrent_file_path, destination_folder
+            print 'Copying from', de, 'to', a
+            shutil.copy(de, a)
+
+            destination_folder += '/' + torrent_info['info']['name']
+
+            print 'Creating directory', destination_folder
+            os.makedirs(destination_folder)
+
+            for a, de in zip(torrent_info['info']['files'], possibility):
+                a = a['path'][0]
+                a = destination_folder + '/' + a
+                print 'Copying from', de, 'to', a
+                shutil.copyfile(de, a)
+
+            return
+
+
+def find_potential_files(path_glob, file_size, threshold=0):
+    return [x for x in glob.glob(path_glob) if abs(os.path.getsize(x) - file_size) <= threshold]
+
+
 torrent_file_path = os.path.abspath(sys.argv[1])
 search_glob = '/mnt/disk-2/bib-backup/*.epub'
+multi_glob = '/mnt/disk-2/bib-backup/*/*'
 archive_root = '/mnt/disk-2/bib-archive/'
 
 
@@ -53,13 +121,13 @@ torrent_dict = decode(torrent_file_path)
 print 'Name:', torrent_dict['info']['name']
 
 if 'length' not in torrent_dict['info']:
-    # todo: fix
-    print 'Torrents with more than one file are not yet supported.'
+    handle_multi_torrent(torrent_dict, multi_glob, archive_path)
     sys.exit()
 
 
-potential_files = [x for x in glob.glob(search_glob) if abs(os.path.getsize(
-    x) - torrent_dict['info']['length']) < 99]
+potential_files = find_potential_files(
+    search_glob, torrent_dict['info']['length'], 99)
+
 
 num_matches = len(potential_files)
 print 'Found %d potential file%s.' % (num_matches, 's' if num_matches != 1 else '')
